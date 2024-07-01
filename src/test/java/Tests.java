99e9ae4c360f9ef1df81cdd4f272ecea6dfd298b
Полина Рыжова <168196240+Astiriya@users.noreply.github.com>
2024-07-01 23:43:33 +0300

    Second

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Main {
+public class Tests {
 
     private static WebDriver driver;
     private static WebDriverWait wait;
